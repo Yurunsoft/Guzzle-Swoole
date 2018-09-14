9c301857b1f1dbf88bcb585acaa99de0c63b3336
宇润 <admin@yurunsoft.com>
2018-09-14 17:31:14 +0800

    完美实现引入项目后的Guzzle全局默认处理器设置

--- src/Plugin/Plugin.php
+++ src/Plugin/Plugin.php
@@ -3,6 +3,7 @@ namespace Yurun\Util\Swoole\Guzzle\Plugin;
 
 use Composer\Composer;
 use Composer\IO\IOInterface;
+use Composer\Util\Filesystem;
 use Composer\Plugin\PluginEvents;
 use Composer\Plugin\PluginInterface;
 use Yurun\Util\Swoole\Guzzle\Plugin\FileGenerator;
@@ -16,9 +17,21 @@ class Plugin implements PluginInterface, EventSubscriberInterface
 	protected $composer;
 
 	/**
-	 * @var \Yurun\Util\Swoole\Guzzle\Plugin\FileGenerator
+	 * @var \Composer\IO\IOInterface
 	 */
-	protected $generator;
+	protected $io;
+
+	/**
+	 * 是否为开发模式
+	 *
+	 * @var boolean
+	 */
+	protected $dev = false;
+
+	public function __construct($dev = false)
+	{
+		$this->dev = $dev;
+	}
 
 	/**
 	 * Apply plugin modifications to Composer
@@ -29,7 +42,7 @@ class Plugin implements PluginInterface, EventSubscriberInterface
 	public function activate(Composer $composer, IOInterface $io)
 	{
 		$this->composer = $composer;
-		$this->generator = new FileGenerator($composer->getEventDispatcher(), $io);
+		$this->io = $io;
 	}
 
 	/**
@@ -44,13 +57,110 @@ class Plugin implements PluginInterface, EventSubscriberInterface
 
 	public function dumpFiles()
 	{
-		$this->generator->dumpLoadFile($this->composer, dirname(__DIR__) . '/load.php');
+		$this->parseGuzzle();
+
+		if(!$this->dev)
+		{
+			$this->appendIncludeFiles();
+		}
 	}
 
 	public static function dev(\Composer\Script\Event $event)
 	{
-		$plugin = new static;
+		$plugin = new static(true);
 		$plugin->activate($event->getComposer(), $event->getIO());
 		$plugin->dumpFiles();
+	}
+
+	/**
+	 * 处理Guzzle代码
+	 *
+	 * @return void
+	 */
+	protected function parseGuzzle()
+	{
+		$filePath = dirname(__DIR__) . '/load.php';
+		$config = $this->composer->getConfig();
+
+		$filesystem = new Filesystem();
+		$filesystem->ensureDirectoryExists($config->get('vendor-dir'));
+		$vendorPath = $filesystem->normalizePath(realpath(realpath($config->get('vendor-dir'))));
+		$autoloadFilesFile = $vendorPath.'/composer/autoload_files.php';
+
+		$files = include $autoloadFilesFile;
+		
+		foreach($files as $fileName)
+		{
+			if($this->stringEndwith($fileName, '/guzzlehttp/guzzle/src/functions_include.php'))
+			{
+				$path = dirname($fileName) . '/functions.php';
+				include $path;
+				$refFunction = new \ReflectionFunction('GuzzleHttp\choose_handler');
+				$content = file_get_contents($path);
+				$eol = $this->getEOL($content);
+				$contents = explode($eol, $content);
+				for($i = $refFunction->getStartLine() - 1; $i < $refFunction->getEndLine(); ++$i)
+				{
+					unset($contents[$i]);
+				}
+				$content = implode($eol, $contents);
+				file_put_contents($filePath, $content);
+				break;
+			}
+		}
+	}
+
+	/**
+	 * 追加 include 文件
+	 *
+	 * @return void
+	 */
+	protected function appendIncludeFiles()
+	{
+		$generator = new \ComposerIncludeFiles\Composer\AutoloadGenerator($this->composer->getEventDispatcher(), $this->io);
+
+		$config = $this->composer->getConfig();
+		$filesystem = new Filesystem();
+		$filesystem->ensureDirectoryExists($config->get('vendor-dir'));
+		$vendorPath = $filesystem->normalizePath(realpath(realpath($config->get('vendor-dir'))));
+
+		$generator->dumpFiles($this->composer, [
+			$vendorPath . "/yurunsoft/guzzle-swoole/src/load.php",
+			$vendorPath . "/yurunsoft/guzzle-swoole/src/functions.php"
+		]);
+	}
+	
+	/**
+	 * 字符串是否以另一个字符串结尾
+	 * @param string $string
+	 * @param string $compare
+	 * @return string
+	 */
+	protected function stringEndwith($string, $compare)
+	{
+		return substr($string, -strlen($compare)) === $compare;
+	}
+
+	/**
+	 * 获取换行符
+	 *
+	 * @param string $content
+	 * @return string
+	 */
+	protected function getEOL($content)
+	{
+		static $eols = [
+			"\r\n",
+			"\n",
+			"\r",
+		];
+		foreach($eols as $eol)
+		{
+			if(strpos($content, $eol))
+			{
+				return $eol;
+			}
+		}
+		return PHP_EOL;
 	}
 }
