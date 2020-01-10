8d6d4f5cf8fa1a8b7bf6f95fe3db3383ee9c74b2
Yurun <admin@yurunsoft.com>
2020-01-10 16:43:50 +0800

    使用 YurunHttp 作为 Http 请求驱动

--- src/SwooleHandler.php
+++ src/SwooleHandler.php
@@ -1,28 +1,15 @@
 <?php
 namespace Yurun\Util\Swoole\Guzzle;
 
-use GuzzleHttp\RequestOptions;
-use Swoole\Coroutine\Http\Client;
+use Yurun\Util\YurunHttp;
+use Yurun\Util\YurunHttp\Attributes;
 use Psr\Http\Message\RequestInterface;
+use Yurun\Util\YurunHttp\Http\Psr7\Uri;
 use GuzzleHttp\Promise\FulfilledPromise;
-use GuzzleHttp\Psr7\Uri;
+use PHPUnit\Framework\Constraint\Attribute;
 
 class SwooleHandler
 {
-    /**
-     * Swoole 协程 Http 客户端
-     *
-     * @var \Swoole\Coroutine\Http\Client
-     */
-    private $client;
-
-    /**
-     * 配置选项
-     *
-     * @var array
-     */
-    private $settings = [];
-
     /**
      * Sends an HTTP request.
      *
@@ -33,109 +20,49 @@ class SwooleHandler
      */
     public function __invoke(RequestInterface $request, array $options)
     {
-        $uri = $request->getUri();
-        $port = $uri->getPort();
-        if(null === $port)
+        $yurunRequest = new \Yurun\Util\YurunHttp\Http\Request($request->getUri(), $request->getHeaders(), (string)$request->getBody(), $request->getMethod(), $request->getProtocolVersion());
+        $yurunRequest = $yurunRequest->withoutHeader('Content-Length')
+                                     // 是否验证 CA
+                                     ->withAttribute(Attributes::IS_VERIFY_CA, $options['verify'])
+                                     // 禁止重定向
+                                     ->withAttribute(Attributes::FOLLOW_LOCATION, false)
+                                     // 超时
+                                     ->withAttribute(Attributes::TIMEOUT, $options['timeout'] ?? null)
+                                     // 连接超时
+                                     ->withAttribute(Attributes::CONNECT_TIMEOUT, $options['connect_timeout'] ?? null);
+        // 用户名密码认证处理
+        $auth = isset($options['auth']) ? $options['auth'] : [];
+        if(isset($auth[1]))
         {
-            if('https' === $uri->getScheme())
-            {
-                $port = 443;
-            }
-            else
-            {
-                $port = 80;
-            }
+            list($username, $password) = $auth;
+            $auth = base64_encode($username . ':' . $password);
+            $yurunRequest = $yurunRequest->withAddedHeader('Authorization', 'Basic ' . $auth);
         }
-
-        $this->client = new Client($uri->getHost(), $port, 'https' === $uri->getScheme());
-
-        // method
-        $this->client->setMethod($request->getMethod());
-
-        // body
-        $this->client->setData((string)$request->getBody());
-
-        // headers
-        $headers = [];
-        foreach($request->getHeaders() as $name => $value)
+        if(!$yurunRequest->hasHeader('Content-Type'))
         {
-            $headers[$name] = implode(',', $value);
+            $yurunRequest = $yurunRequest->withHeader('Content-Type', '');
         }
-        // 带有 Content-Length 时，少数奇葩服务器会无法顺利接收 post 数据
-        if(isset($headers['Content-Length']))
-        {
-            unset($headers['Content-Length']);
-        }
-        $this->client->setHeaders($headers);
-
-        // 其它处理
-        $this->parseSSL($request, $options);
-        $this->parseProxy($request, $options);
-        $this->parseNetwork($request, $options);
-
-        // 设置客户端参数
-        if(!empty($this->settings))
-        {
-            $this->client->set($this->settings);
-        }
-
-        // 发送
-        $path = $uri->getPath();
-        if('' === $path)
-        {
-            $path = '/';
-        }
-        $query = $uri->getQuery();
-        if('' !== $query)
-        {
-            $path .= '?' . $query;
-        }
-
-        $this->client->execute($path);
-
-        $response = $this->getResponse();
-
-        return new FulfilledPromise($response);
-    }
-
-    private function parseSSL(RequestInterface $request, array $options)
-    {
-        if(($verify = $options['verify']))
-        {
-            $this->settings['ssl_verify_peer'] = true;
-            if(is_string($verify))
-            {
-                $this->settings['ssl_cafile'] = $verify;
-            }
-        }
-        else
-        {
-            $this->settings['ssl_verify_peer'] = false;
-        }
-
+        // 证书
         $cert = isset($options['cert']) ? $options['cert'] : [];
         if(isset($cert[0]))
         {
-            $this->settings['ssl_cert_file'] = $cert[0];
+            $yurunRequest = $yurunRequest->withAttribute(Attributes::CERT_PATH, $cert[0]);
         }
-        else if(isset($this->settings['ssl_cert_file']))
+        if(isset($cert[1]))
         {
-            unset($this->settings['ssl_cert_file']);
+            $yurunRequest = $yurunRequest->withAttribute(Attributes::CERT_PASSWORD, $cert[1]);
         }
-
-        $key = isset($options['key']) ? $options['key'] : [];
+        // ssl_key
+        $key = isset($options['ssl_key']) ? $options['ssl_key'] : [];
         if(isset($key[0]))
         {
-            $this->settings['ssl_key_file'] = $key[0];
+            $yurunRequest = $yurunRequest->withAttribute(Attributes::KEY_PATH, $key[0]);
         }
-        else if(isset($this->settings['ssl_key_file']))
+        if(isset($key[1]))
         {
-            unset($this->settings['ssl_key_file']);
+            $yurunRequest = $yurunRequest->withAttribute(Attributes::KEY_PASSWORD, $key[1]);
         }
-    }
-
-    private function parseProxy(RequestInterface $request, array $options)
-    {
+        // 代理
         $proxy = isset($options['proxy']) ? $options['proxy'] : [];
         if(is_string($proxy))
         {
@@ -143,71 +70,50 @@ class SwooleHandler
                 'http' => $proxy,
             ];
         }
-        if(isset($proxy['no']) && \GuzzleHttp\is_host_in_noproxy($request->getUri()->getHost(), $proxy['no']))
+        if(!(isset($proxy['no']) && \GuzzleHttp\is_host_in_noproxy($request->getUri()->getHost(), $proxy['no'])))
         {
-            if(isset($this->settings['http_proxy_host']))
+            $scheme = $request->getUri()->getScheme();
+            $proxyUri = isset($proxy[$scheme]) ? $proxy[$scheme] : null;
+            if(null !== $proxyUri)
             {
-                unset($this->settings['http_proxy_host'], $this->settings['http_proxy_port'], $this->settings['http_proxy_user'], $this->settings['http_proxy_password']);
+                $proxyUri = new Uri($proxyUri);
+                $userinfo = explode(':', $proxyUri->getUserInfo());
+                if(isset($userinfo[1]))
+                {
+                    list($username, $password) = $userinfo;
+                }
+                else
+                {
+                    $username = $userinfo[0];
+                    $password = '';
+                }
+                $yurunRequest = $yurunRequest->withAttribute(Attributes::PROXY_SERVER, $proxyUri->getHost())
+                                             ->withAttribute(Attributes::PROXY_PORT, $proxyUri->getPort())
+                                             ->withAttribute(Attributes::PROXY_USERNAME, $username)
+                                             ->withAttribute(Attributes::PROXY_PASSWORD, $password);
             }
-            return;
         }
-        $scheme = $request->getUri()->getScheme();
-        $proxyUri = isset($proxy[$scheme]) ? $proxy[$scheme] : null;
-        if(null === $proxyUri)
-        {
-            if(isset($this->settings['http_proxy_host']))
-            {
-                unset($this->settings['http_proxy_host'], $this->settings['http_proxy_port'], $this->settings['http_proxy_user'], $this->settings['http_proxy_password']);
-            }
-            return;
-        }
-        $proxyUri = new Uri($proxyUri);
-        $userinfo = explode(':', $proxyUri->getUserInfo());
-        if(isset($userinfo[1]))
-        {
-            list($username, $password) = $userinfo;
-        }
-        else
-        {
-            $username = $userinfo[0];
-            $password = '';
-        }
-        $this->settings['http_proxy_host'] = $proxyUri->getHost();
-        $this->settings['http_proxy_port'] = $proxyUri->getPort();
-        $this->settings['http_proxy_user'] = $username;
-        $this->settings['http_proxy_password'] = $password;
+        // 发送请求
+        $yurunResponse = YurunHttp::send($yurunRequest, \Yurun\Util\YurunHttp\Handler\Swoole::class);
+        $response = $this->getResponse($yurunResponse);
+        return new FulfilledPromise($response);
     }
 
-    private function parseNetwork(RequestInterface &$request, array $options)
+    /**
+     * 获取 Guzzle Response
+     *
+     * @param \Yurun\Util\YurunHttp\Http\Response $yurunResponse
+     * @return \GuzzleHttp\Psr7\Response
+     */
+    private function getResponse($yurunResponse)
     {
-        // 用户名密码认证处理
-        $auth = isset($options['auth']) ? $options['auth'] : [];
-        if(isset($auth[1]))
+        $headers = [];
+        foreach($yurunResponse->getHeaders() as $name => $str)
         {
-            list($username, $password) = $auth;
-            $auth = base64_encode($username . ':' . $password);
-            $request = $request->withAddedHeader('Authorization', 'Basic ' . $auth);
+            $headers[$name] = implode(', ', $str);
         }
-        // 超时
-        if(isset($options['timeout']) && $options['timeout'] > 0)
-        {
-            $this->settings['timeout'] = $options['timeout'];
-        }
-        else
-        {
-            $this->settings['timeout'] = -1;
-        }
-    }
-
-    private function getResponse()
-    {
-        $headers = isset($this->client->headers) ? $this->client->headers : [];
-        if(isset($headers['set-cookie']))
-        {
-            $headers['set-cookie'] = $this->client->set_cookie_headers;
-        }
-        // var_dump($this->client);
-        $response = new \GuzzleHttp\Psr7\Response($this->client->statusCode, $headers, $this->client->body);
+        $response = new \GuzzleHttp\Psr7\Response($yurunResponse->getStatusCode(), $headers, $yurunResponse->getBody());
         return $response;
     }
+
 }
