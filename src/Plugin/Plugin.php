4ee65bc42f39a074799189a1f367a080182621cf
Yurun <admin@yurunsoft.com>
2020-10-25 18:21:19 +0800

    修复问题

--- src/Plugin/Plugin.php
+++ src/Plugin/Plugin.php
@@ -4,9 +4,7 @@ namespace Yurun\Util\Swoole\Guzzle\Plugin;
 use Composer\Composer;
 use Composer\IO\IOInterface;
 use Composer\Util\Filesystem;
-use Composer\Plugin\PluginEvents;
 use Composer\Plugin\PluginInterface;
-use Yurun\Util\Swoole\Guzzle\Plugin\FileGenerator;
 use Composer\EventDispatcher\EventSubscriberInterface;
 
 class Plugin implements PluginInterface, EventSubscriberInterface
@@ -43,6 +41,34 @@ class Plugin implements PluginInterface, EventSubscriberInterface
     {
         $this->composer = $composer;
         $this->io = $io;
+    }
+
+    /**
+     * Remove any hooks from Composer
+     *
+     * This will be called when a plugin is deactivated before being
+     * uninstalled, but also before it gets upgraded to a new version
+     * so the old one can be deactivated and the new one activated.
+     *
+     * @param Composer    $composer
+     * @param IOInterface $io
+     */
+    public function deactivate(Composer $composer, IOInterface $io)
+    {
+
+    }
+
+    /**
+     * Prepare the plugin to be uninstalled
+     *
+     * This will be called after deactivate.
+     *
+     * @param Composer    $composer
+     * @param IOInterface $io
+     */
+    public function uninstall(Composer $composer, IOInterface $io)
+    {
+
     }
 
     /**
