8f4e8a6d067b67df703939a18842f1631e984e1d
Yurun <admin@yurunsoft.com>
2020-11-24 16:22:01 +0800

    增加 swoole master 分支测试

---
+++ .github/prepare-test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+__DIR__=$(cd `dirname $0`; pwd)
+
+cd $__DIR__
+
+docker-compose up -d $1
+&& docker exec $1 php -v
+&& docker exec $1 php --ri $1
+&& docker exec $1 composer -V
+&& docker ps -a
+&& docker exec $1 composer update
