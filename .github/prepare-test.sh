de437c2c61b0d7d9c11738d2ae780ff1dcdad050
Yurun <admin@yurunsoft.com>
2020-11-24 16:31:00 +0800

    修复

--- .github/prepare-test.sh
+++ .github/prepare-test.sh
@@ -4,9 +4,9 @@ __DIR__=$(cd `dirname $0`; pwd)
 
 cd $__DIR__
 
-docker-compose up -d $1
-&& docker exec $1 php -v
-&& docker exec $1 php --ri swoole
-&& docker exec $1 composer -V
-&& docker ps -a
+docker-compose up -d $1 \
+&& docker exec $1 php -v \
+&& docker exec $1 php --ri swoole \
+&& docker exec $1 composer -V \
+&& docker ps -a \
 && docker exec $1 composer update
