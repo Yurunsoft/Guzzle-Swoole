c8f201654e874cea45b6872ead971d0815820c64
Yurun <admin@yurunsoft.com>
2022-01-10 16:42:43 +0800

    修复引入时报错问题

--- src/load_include.php
+++ src/load_include.php
@@ -1,6 +1,6 @@
 <?php
 
-if (!function_exists('GuzzleHttp\uri_template') && !function_exists('GuzzleHttp\describe_type'))
+if (!function_exists('GuzzleHttp\uri_template') && !function_exists('GuzzleHttp\describe_type') && !class_exists(\GuzzleHttp\Utils::class, false))
 {
     $file = __DIR__ . '/load.php';
     if (is_file($file))
